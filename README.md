865e7d8e86f71bebdd750a652894a1cc61318cae
Dalerjon500 <dalerjonismatov500@gmail.com>
2026-05-02 11:13:54 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# course_center_back
--- seed.py
+++ seed.py
@@ -1,7 +1,6 @@
 from app.core.database import SessionLocal
 from app.core.security import hash_password
 from app.models.enums import UserRole, UserStatus
-from app.models.room import Room
 from app.models.user import User
 
 
@@ -17,6 +16,7 @@ def upsert_user(
 ) -> User:
     normalized_email = email.strip().lower()
     user = db.query(User).filter(User.email == normalized_email).first()
+
     if user is None:
         user = User(email=normalized_email)
         db.add(user)
@@ -27,40 +27,50 @@ def upsert_user(
     user.password_hash = hash_password(password)
     user.roles = roles
     user.status = status
+
     return user
 
 
 def seed_data() -> None:
     db = SessionLocal()
     try:
-        # Admin
-        admin = upsert_user(
+        upsert_user(
             db,
-            email="muslima@gmail.com",
-            full_name="Muslima Radjabova",
-            phone="+998912345678",
+            email="daler@gmail.com",
+            full_name="Daler Ismatov",
+            phone="+998947668884",
             password="12345678",
             roles=[UserRole.ADMIN, UserRole.TEACHER],
         )
 
-        # Hasan Rasulov
-        hasan = upsert_user(
+        upsert_user(
             db,
             email="rasulov420@gmail.com",
             full_name="Hasan Rasulov",
             phone="+998931373027",
             password="qwerty123",
-            roles=[UserRole.ADMIN, UserRole.TEACHER],  # xohlasang USER yoki boshqa role qilamiz
+            roles=[UserRole.ADMIN, UserRole.TEACHER],
+        )
+
+        upsert_user(
+            db,
+            email="dalerjon@gmail.com",
+            full_name="Dalerjon Admin",
+            phone="+998900000000",
+            password="12345678d",
+            roles=[UserRole.ADMIN, UserRole.TEACHER],
         )
 
         db.commit()
 
         print("Seed completed successfully.")
-        print("Admin: muslima@gmail.com / 12345678")
-        print("Hasan: rasulov42@gmail.com / qwerty123")
+        print("Admin: daler@gmail.com  / 12345678")
+        print("Hasan: rasulov420@gmail.com / qwerty123")
+        print("Dalerjon: dalerjon@gmail.com / 12345678d")
 
-    except Exception:
+    except Exception as e:
         db.rollback()
+        print("Error:", e)
         raise
     finally:
         db.close()
